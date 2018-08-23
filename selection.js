02cc6cef8066ff0ecba60a43cf4c3505961b8d10
Ana C Sanchez <ana.sanchez6547@gmail.com>
2018-08-23 16:39:56 -0400

    moved options, config into a single object, changed Button to bindOptionsToNewButton

--- selection.js
+++ selection.js
@@ -77,7 +77,7 @@ const Selection = (function() {
       twitter: {
         url: 'https://twitter.com/intent/tweet?text=',
         icon: '<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" class="selection__icon"><path d="M8.2,20.2c6.5,0,11.7-5.2,11.8-11.6c0-0.1,0-0.1,0-0.2c0-0.2,0-0.4,0-0.5c0.8-0.6,1.5-1.3,2.1-2.2c-0.8,0.3-1.6,0.6-2.4,0.7c0.9-0.5,1.5-1.3,1.8-2.3c-0.8,0.5-1.7,0.8-2.6,1c-1.6-1.7-4.2-1.7-5.9-0.1c-1.1,1-1.5,2.5-1.2,3.9C8.5,8.7,5.4,7.1,3.3,4.6c-1.1,1.9-0.6,4.3,1.3,5.5c-0.7,0-1.3-0.2-1.9-0.5l0,0c0,2,1.4,3.7,3.3,4.1c-0.6,0.2-1.2,0.2-1.9,0.1c0.5,1.7,2.1,2.8,3.9,2.9c-1.7,1.4-3.9,2-6.1,1.7C3.8,19.5,6,20.2,8.2,20.2"/></svg>',
-        action: function() {
+        onclick: function() {
           popupwindow(this.url + encodeURIComponent(text) + ' ' + windowUrl, 'Share', 550, 295);
           return false;
         }
@@ -86,7 +86,7 @@ const Selection = (function() {
         url: 'https://www.facebook.com/sharer/sharer.php?quote=',
         icon:
           '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" enable-background="new 0 0 24 24" width="24" height="24" class="selection__icon"><path d="M20,2H4C2.9,2,2,2.9,2,4v16c0,1.1,0.9,2,2,2h16c1.1,0,2-0.9,2-2V4C22,2.9,21.1,2,20,2z M18.4,7.4H17c-0.9,0-1,0.3-1,1l0,1.3 h2.1L18,12h-1.9v7h-3.2v-7h-1.2V9.6h1.2V8.1c0-2,0.8-3.1,3.1-3.1h2.4V7.4z"/></svg>',
-        action: function() {
+        onclick: function() {
           let sharelink = windowUrl;
           let finalurl = this.url;
           if (sharelink.substring(0, 4) !== 'http') {
@@ -100,7 +100,7 @@ const Selection = (function() {
         url: 'https://www.google.co.in/search?q=',
         icon:
           '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" enable-background="new 0 0 24 24" width="24" height="24" class="selection__icon"><path d="M23.111 20.058l-4.977-4.977c.965-1.52 1.523-3.322 1.523-5.251 0-5.42-4.409-9.83-9.829-9.83-5.42 0-9.828 4.41-9.828 9.83s4.408 9.83 9.829 9.83c1.834 0 3.552-.505 5.022-1.383l5.021 5.021c2.144 2.141 5.384-1.096 3.239-3.24zm-20.064-10.228c0-3.739 3.043-6.782 6.782-6.782s6.782 3.042 6.782 6.782-3.043 6.782-6.782 6.782-6.782-3.043-6.782-6.782zm2.01-1.764c1.984-4.599 8.664-4.066 9.922.749-2.534-2.974-6.993-3.294-9.922-.749z"/></svg>',
-        action: function() {
+        onclick: function() {
           popupwindow(this.url + encodeURIComponent(text), 'Search', 900, 540);
           return false;
         }
@@ -108,7 +108,7 @@ const Selection = (function() {
       copy: {
         icon:
           '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" enable-background="new 0 0 24 24" width="24" height="24" class="selection__icon"><path d="M18 6v-6h-18v18h6v6h18v-18h-6zm-12 10h-4v-14h14v4h-10v10zm16 6h-14v-14h14v14z"/></svg>',
-        action: function() {
+        onclick: function() {
           copyTextToClipboard(text);
         }
       },
@@ -125,7 +125,7 @@ const Selection = (function() {
           '<path id="svg_17" d="m16,17c-0.157,0 -0.311,-0.073 -0.408,-0.21c-0.16,-0.225 -0.107,-0.537 0.118,-0.697c2.189,-1.555 3.79,-4.727 3.79,-5.592c0,-0.276 0.224,-0.5 0.5,-0.5s0.5,0.224 0.5,0.5c0,1.318 -1.927,4.785 -4.21,6.408c-0.088,0.061 -0.189,0.091 -0.29,0.091z"/>'+
           '<path id="svg_19" d="m20,18c-0.121,0 -0.242,-0.043 -0.337,-0.131c-0.363,-0.332 -3.558,-3.283 -4.126,-4.681c-0.104,-0.256 0.02,-0.547 0.275,-0.651c0.253,-0.103 0.547,0.019 0.651,0.275c0.409,1.007 2.936,3.459 3.875,4.319c0.204,0.187 0.217,0.502 0.031,0.707c-0.099,0.107 -0.234,0.162 -0.369,0.162z"/>'+
           '</svg>',
-        action: function() {
+        onclick: function() {
           popupwindow(this.url + getBrowserLanguage() + '/' + text, 'Translate', 900, 540);
           return false;
         }
@@ -133,11 +133,20 @@ const Selection = (function() {
       speak: {
         icon:
           '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" enable-background="new 0 0 24 24" width="24" height="24" class="selection__icon"><path d="M16 11c0 2.209-1.791 4-4 4s-4-1.791-4-4v-7c0-2.209 1.791-4 4-4s4 1.791 4 4v7zm4-2v2c0 4.418-3.582 8-8 8s-8-3.582-8-8v-2h2v2c0 3.309 2.691 6 6 6s6-2.691 6-6v-2h2zm-7 13v-2h-2v2h-4v2h10v-2h-4z"/></svg>',
-        action: function() {
+        onclick: function() {
           let speech = new SpeechSynthesisUtterance(text);
           window.speechSynthesis.speak(speech);
         }
       }
+    }
+
+    function bindOptionsToNewButton(icon, option) {
+      return Object.assign(document.createElement('div'), {
+        style: `display:inline-block; margin:7px; cursor:pointer; transition:all .2s ease-in-out; fill:${fill};`,
+        innerHTML: icon,
+        onmouseover: function() { this.style.transform = 'scale(1.2)'; },
+        onmouseout: function() { this.style.transform = 'scale(1)'; }
+      }, option)
     }
 
     function appendIcons() {
@@ -147,7 +156,7 @@ const Selection = (function() {
       Object.keys(menuOptions).forEach((option) => {
         const { icon } = menuOptions[option];
         if (menu[option]) {
-          const btn = new Button(icon, menuOptions[option]);
+          const btn = bindOptionsToNewButton(icon, menuOptions[option]);
           div.appendChild(btn);
           count++;
         }
@@ -255,16 +264,6 @@ const Selection = (function() {
       config: config,
       init: init
     };
-
-    function Button(icon, option) {
-      return Object.assign(document.createElement('div'), {
-        style: `display:inline-block; margin:7px; cursor:pointer; transition:all .2s ease-in-out; fill:${fill};`,
-        innerHTML: icon,
-        onclick: option.action.bind(option),
-        onmouseover: function() { this.style.transform = 'scale(1.2)'; },
-        onmouseout: function() { this.style.transform = 'scale(1)'; }
-      })
-    }
   }
 
   return _selection;
